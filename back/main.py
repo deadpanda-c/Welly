db21a35ff3d00689f0a590216722aa5b8fc01b9f
Clément Lagier <clement.lagier@epitech.eu>
2024-10-24 10:40:30 +0400

    [ADD] Added basic pages routes

--- back/main.py
+++ back/main.py
@@ -1,12 +1,35 @@
 #!./.venv/bin/python3
 
 import flask
+import json
 
+"""
+Routes and views for the flask application.
+
+- / : Home page
+
+- /ai : Chatbot page
+
+- /home : Home page
+
+-
+"""
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
+    # get the current user informations
+
+
     return 'Hello, World!'
+
+@app.route("/ai")
+def chat():
+    return "Chatbot"
+
+@app.route("/home")
+def home():
+    return "Home page"
 
 if __name__ == '__main__':
     app.run(debug=True)
