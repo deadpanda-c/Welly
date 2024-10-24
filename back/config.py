beccdd0fbd848dbb750d1831076704e359951d0e
Grainbox <tagemania@gmail.com>
2024-10-24 22:41:12 +0900

    [Feat]: goal routes

--- back/config.py
+++ back/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///welly.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'my-secret-key')
--- back/routes/goal_routes.py
+++ back/routes/goal_routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.goal import Goal
+from models.user import User
+from models.favorite_goal import FavoriteGoal
 from extensions import db
 
 goal_bp = Blueprint('goal_bp', __name__)
@@ -33,3 +36,44 @@ def get_goal():
         return jsonify({"message": "Goal already exists"}), 400
 
     return jsonify({'title': goal.title, 'description': goal.description, 'minigoal': goal.minigoal})
+
+@goal_bp.route("/add_favorite", methods=["POST"])
+@jwt_required()
+def add_favorite():
+    data = request.get_json()
+    current_user = get_jwt_identity()
+
+    user = User.query.filter_by(username=current_user).first()
+
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    goal_id = data["goal_id"]
+    new_favorite = FavoriteGoal(user_id=user.id, goal_id=goal_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+
+    return jsonify({'favorite': new_favorite.id})
+
+@goal_bp.route("/remove_favorite", methods=["POST"])
+@jwt_required()
+def remove_favorite():
+    data = request.get_json()
+    current_user = get_jwt_identity()
+
+    user = User.query.filter_by(username=current_user).first()
+
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    goal_id = data["goal_id"]
+
+    favorite_goal = FavoriteGoal.query.filter_by(user_id=user.id, goal_id=goal_id).first()
+
+    if not favorite_goal:
+        return jsonify({"message": "Favorite goal not found"}), 404
+
+    db.session.delete(favorite_goal)
+    db.session.commit()
+
+    return jsonify({"message": "Favorite goal removed successfully"}), 200
