5aa2d7a5cb0c71ca4e84cd2f8f9628014e4716f7
Grainbox <tagemania@gmail.com>
2024-10-24 20:10:02 +0900

    [Feat]: Authentication routes

--- back/routes/ai_routes.py
+++ back/routes/ai_routes.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from ollama_llm import ollama_llm
 
 ai_bp = Blueprint('ai_bp', __name__)
 
 @ai_bp.route('/ai', methods=['POST'])
+@jwt_required()
 def chat():
+
+    current_user = get_jwt_identity()
+
+    print(current_user)
+
     data = request.get_json()
 
-    # Interroger le modèle AI
     response = ollama_llm(data["content"])
 
     return jsonify({'chat': response})
--- back/routes/auth_routes.py
+++ back/routes/auth_routes.py
@@ -8,18 +8,34 @@ auth_bp = Blueprint("auth_bp", __name__)
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
+    email = data.get("email")
     username = data.get("username")
     password = data.get("password")
 
-    if User.query.filter_by(username=username).first():
+    if User.query.filter_by(email=email).first():
         return jsonify({"message": "User already exists"}), 400
 
     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
 
-    new_user = User(username=username, password=hashed_password)
+    new_user = User(email=email, username=username, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
 
     access_token = create_access_token(identity=username)
 
     return jsonify({"access_token": access_token}), 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user or not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"message": "Incorrect username or password"}), 403
+
+    access_token = create_access_token(identity=username)
+
+    return jsonify({"access_token": access_token}), 200
