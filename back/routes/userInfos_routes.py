c5b053d780a8b26974853778fca04ecd2e33b3f7
Clément Lagier <clement.lagier@epitech.eu>
2024-10-24 17:39:48 +0400

    [ADD] Get the infos of the user once he is registered

--- back/routes/userInfos_routes.py
+++ back/routes/userInfos_routes.py
@@ -1,12 +1,26 @@
 #!../.venv/bin/python3
 
 from flask import Blueprint, request, jsonify
-import utils
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+import routes.utils as utils
 
 userInfos_bp = Blueprint('userInfos', __name__)
-sqlite = utils.get_module_sqlite("./test.db")
+
+@userInfos_bp.route('/userInfos', methods=['GET'])
+@jwt_required()
+def get_user_infos():
+    current_user_id = get_jwt_identity()[0]
+    sqlite = utils.SqliteModule("./instance/users.db")
+
+    user_infos = sqlite.get_row("user", {"id": current_user_id})
+
+    print(user_infos)
+    sqlite.close_connection()
+    return jsonify({"msg": "Hello, World!"})
+
 
 if __name__ == "__main__":
-    # get only one user
-    print(sqlite.get_row("Users", {"id": 1}))
+    pass
+
 
